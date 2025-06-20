b49ea9d6494af0946606a9c951df539348a49645
ROG <varuntheace@gmail.com>
2025-06-20 08:36:54 +0530

    I forgot to do git add last time ;-;

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ st.set_page_config(page_title="Content Generator", layout="centered")
 
 st.markdown("<b><h2 style='text-align: center;'>Content Generator</h2></b>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center;'>Generate a consolidated file from repository or local directory contents</p>", unsafe_allow_html=True)
+
+tab1, tab2 = st.tabs(["GitHub Repository", "Local Directory"])
+
+with tab1:
+    repo_url = st.text_input("Repository URL", 
+                             placeholder="https://github.com/username/repo")
+    if st.button("Generate README", key="repo"):
+        code_content = "Hello World!"
+        st.text_area("Generated README", code_content, height=400)
+        st.button("Copy Content")
